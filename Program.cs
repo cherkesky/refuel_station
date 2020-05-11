8eb6adf22428ac39c18698858078d943be060fc2
Cherkesky <guy@cherkesky.com>
2020-05-11 17:42:51 -0500

    instanciating the stations

--- Program.cs
+++ Program.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using System.Collections.Generic;
 
 namespace refuel_station
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+
+      Zero fxs = new Zero();
+      Zero fx = new Zero();
+      Tesla modelS = new Tesla();
+      List<IElectricPowered> electricVehicles = new List<IElectricPowered>();
+
+      electricVehicles.Add(fx);
+      electricVehicles.Add(fxs);
+      electricVehicles.Add(modelS);
+
+      Ram ram = new Ram();
+      Cessna cessna150 = new Cessna();
+      List<IGasPowered> gasVehicles = new List<IGasPowered>() {
+                ram, cessna150
+            };
+
+      GasStation nashvilleGas = new GasStation(){Capacity=3};
+      BatteryStation nashvilleBattery = new BatteryStation(){Capacity=3};
+
+
     }
+  }
 }
