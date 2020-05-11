61618a0b4a9872bc3e5ca7f543bf8c0fb022ea1b
Cherkesky <guy@cherkesky.com>
2020-05-11 17:40:45 -0500

    instanciating the stations

--- Models/Interfaces.cs
+++ Models/Interfaces.cs
@@ -15,9 +15,6 @@ namespace refuel_station {
   {
     //  The number of vehicles that they can refuel at any one time.
      int Capacity {get; set;}
-     void Refuel(List<T> vehicles){
-       // "The {Cessna} has been refueled with {200} gallons of gas"
-       System.Console.WriteLine($"{vehicles}");
-     }
+     void Refuel(List<T> vehicles);
   } 
 }
